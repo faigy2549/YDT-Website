6cd8d8013750739d318cb38719c747c7d115bce3
faigygit <faigy2549@gmail.com>
2024-12-15 18:18:42 +0200

    erased irrelavant comps

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
  const routes: Routes = [
-  ];
+ // {path:'',component:},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,29 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ToastModule } from 'primeng/toast';
-import { TableModule } from 'primeng/table';
-import {ToolbarModule} from 'primeng/toolbar';
-import {FileUploadModule} from 'primeng/fileupload';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {ButtonModule} from 'primeng/button';
-import {DialogModule} from 'primeng/dialog';
-import {ConfirmDialogModule} from 'primeng/confirmdialog';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ConfirmationService, MessageService, PrimeIcons } from 'primeng/api';
-import { Observable } from 'rxjs';
-import { TabMenuModule } from 'primeng/tabmenu';
-import { CarouselModule } from 'primeng/carousel';
-import { CheckboxModule } from 'primeng/checkbox';
-import { DropdownModule } from 'primeng/dropdown';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { TokenInterceptor } from './TokenInterceptor.interceptor';
-import { MenuModule } from 'primeng/menu';
-import { MenubarModule } from 'primeng/menubar';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,25 +9,8 @@ import { MenubarModule } from 'primeng/menubar';
   imports: [
    BrowserModule,
    AppRoutingModule,
-  // FormsModule,
-  // ReactiveFormsModule,
-  // ToastModule,
-  // TableModule,
-  // ToolbarModule,
-  // HttpClientModule,
-  // ButtonModule,
-  // DialogModule,
-  // ConfirmDialogModule,
-  // BrowserAnimationsModule,
-  // TabMenuModule,
-  // DataViewModule,
-  // CarouselModule,
-  // DropdownModule,
-  // CheckboxModule,
-  // MenuModule,
-  // MenubarModule,
   ],
-  providers: [MessageService,ConfirmationService, { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
